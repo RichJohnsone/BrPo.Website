cdf5fe687ef5bec2177bba02883290dabd74a44d
Rich Johnstone <RichJohnstone44@gmail.com>
2021-12-07 17:14:15 +0000

    hopepage-added-unobtrusive-ajax

--- BrPo.Website/Pages/Index.cshtml.cs
+++ BrPo.Website/Pages/Index.cshtml.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BrPo.Website.Services.ContactForm.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BrPo.Website.Pages
@@ -12,14 +10,20 @@ namespace BrPo.Website.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty]
+        public ContactModel ContactModel { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
-        {
+        public void OnGet() {}
 
+        public async Task<IActionResult> OnPostSaveFormAsync()
+        {
+            await Task.Delay(20);
+            return Content(ContactModel.Name);
         }
     }
 }
---
+++ BrPo.Website/Services/ContactForm/Models/ContactModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BrPo.Website.Services.ContactForm.Models
+{
+    public class ContactModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
